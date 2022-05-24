8e6873ab08790894f25c0ff9f72c962962091737
alihpss <ali.hps@hotmail.com>
2022-05-24 17:45:04 -0300

    att

--- JS/requisicao.js
+++ JS/requisicao.js
@@ -6,22 +6,33 @@ const elementos = ['Grass', 'Fire', 'Water', 'Bug', 'Normal', 'Poison', 'Electri
 
 const res = document.querySelector('#res')
 
+
+
+
 window.addEventListener('load' , () => {
-    for (let i = 1; i <= 151; i++) {
-        const requisicao = new XMLHttpRequest(); 
+    for (let i = 1; i <= 24; i++) {
+        //const requisicao = new XMLHttpRequest(); 
 
-        requisicao.open('GET' , `https://pokeapi.co/api/v2/pokemon/${i}/`)
-        requisicao.send(); 
+        fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        .then(response => response.json())
+        .then(pokemon => {
 
-        requisicao.addEventListener('load', function () {
-            let resposta = requisicao.responseText;
-
-            respostaJson = JSON.parse(resposta);
-
-            let pokemon = criarPokemon(respostaJson);
-            
-            gerarNovoIcone(divPokemons, pokemon.imagem, pokemon.nome, pokemon.tipo);
+            let atribuiPoke = criarPokemon(pokemon);
+            gerarNovoIcone(divPokemons, atribuiPoke.imagem, atribuiPoke.nome, atribuiPoke.tipo)
         })
+
+        //requisicao.open('GET' , `https://pokeapi.co/api/v2/pokemon/${i}`)
+        //requisicao.send(); 
+//
+        //requisicao.addEventListener('load', function () {
+        //    let resposta = requisicao.responseText;
+        //    console.log(requisicao);
+        //    respostaJson = JSON.parse(resposta);
+        //    //console.log(respostaJson);
+        //    let pokemon = criarPokemon(respostaJson);
+        //    
+        //    gerarNovoIcone(divPokemons, pokemon.imagem, pokemon.nome, pokemon.tipo);
+        //})
     }
 })
 
