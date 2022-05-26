aaac0f07bdf6fe4f36466b8b97339d66f9f8ce66
Ali <ali.hps@hotmail.com>
2022-05-25 22:08:47 -0300

    atualizando requesições

--- JS/icones.js
+++ JS/icones.js
@@ -1,14 +1,18 @@
-const gerarNovoIcone = (menu,tipo, atributoImg, spanNome, spanTipo) => {
+const gerarNovoIcone = (tipo, atributoImg, spanNome, spanTipo) => {
     let div = document.createElement('div');
     let img = document.createElement('img')
     let span1 = document.createElement('span');
     let span2 = document.createElement('span');
     let input = document.createElement('input');
+    let menu = document.getElementById('lista-pokemons')
+
 
     img.setAttribute('src', atributoImg);
     span1.textContent = spanNome;
     span2.textContent = spanTipo;
     input.value = "+ info"
+
+    span1.classList.add('span-name')
     div.classList.add('pokemons');
     div.classList.add(tipo);
 
@@ -17,7 +21,6 @@ const gerarNovoIcone = (menu,tipo, atributoImg, spanNome, spanTipo) => {
     div.appendChild(span1)
     div.appendChild(span2)
     div.appendChild(input)
-
 
     menu.appendChild(div);
 }
--- JS/requisicao.js
+++ JS/requisicao.js
@@ -1,79 +1,45 @@
 const botao = document.querySelector('#listarPokemon');
-let listaPokemons = document.querySelectorAll('.pokemons')
-let divPokemons = document.querySelector('#lista-pokemons');
 
-const elementos = ['Grass', 'Fire', 'Water', 'Bug', 'Normal', 'Poison', 'Electric', 'Ground', 'Fairy', 'Fighting', 'Psychic', 'Rock', 'Ghost', 'Ice', 'Dragon']
+let listaPokemons = document.querySelector('#lista-pokemons');
+
+const elementos = ['Grass', 'Fire', 'Water', 'Bug', 'Normal', 'Poison', 'Electric', 'Ground', 'Fairy', 'Fighting', 'Psychic', 'Rock', 'Ghost', 'Ice', 'Dragon'];
 
 const res = document.querySelector('#res')
 
 
 const requisicaoPokemons = (inicioPersonagens,limitePersonagens) => {
-    const getUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+    const getUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-    const pokemonPromises = []
+    const pokemonPromises = [];
 
     for (let i = inicioPersonagens; i <= limitePersonagens; i++) {
-        pokemonPromises.push(fetch(getUrl(i)).then(response => response.json()))
+        pokemonPromises.push(fetch(getUrl(i)).then(response => response.json()));
         
     }
 
     Promise.all(pokemonPromises)
         .then(pokemons => {
+            
             for (let index = 0; index < pokemonPromises.length; index++) {
                 let pokemon = pokemons[index]
-                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+                const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+                console.log(pokemons[index]);
+                gerarNovoIcone( types[0], pokemon.sprites.front_default, pokemon.name, types.join(' | '));
 
-                gerarNovoIcone(divPokemons, types[0], pokemon.sprites.front_default, pokemon.name, types.join(' | '));
-            }
-            
+                
+                
+               //console.log(teste);
+            };
+
+           // outrasInfo
+
+            let divPokemons = document.querySelectorAll('.pokemons')
+            //console.log(divPokemons[0]);
         })
-
-}
+};
 requisicaoPokemons(1,24);
 
 
 
 
-
-//window.addEventListener('load' , () => {
-    //for (let i = 1; i <= 24; i++) {
-        //const requisicao = new XMLHttpRequest(); 
-
-        //fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-        //.then(response => response.json())
-        //.then(pokemon => {
-
-            //let atribuiPoke = criarPokemon(pokemon);
-            //gerarNovoIcone(divPokemons, atribuiPoke.imagem, atribuiPoke.nome, atribuiPoke.tipo)
-        //})
-
-        //requisicao.open('GET' , `https://pokeapi.co/api/v2/pokemon/${i}`)
-        //requisicao.send(); 
-//
-        //requisicao.addEventListener('load', function () {
-        //    let resposta = requisicao.responseText;
-        //    console.log(requisicao);
-        //    respostaJson = JSON.parse(resposta);
-        //    //console.log(respostaJson);
-        //    let pokemon = criarPokemon(respostaJson);
-        //    
-        //    gerarNovoIcone(divPokemons, pokemon.imagem, pokemon.nome, pokemon.tipo);
-        //})
-    //}
-//})
-
-function criarPokemon(pokemon) {
-
-    let poke = {
-
-    nome: pokemon.name,
-    tipo: pokemon.types[0].type.name,
-    imagem: pokemon.sprites.front_default
-
-    }
-    
-
-
-    return poke;
-}
 
